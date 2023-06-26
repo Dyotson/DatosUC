0ca231c620e4b16cca693b4dc1facef2efdbb212
Maximiliano Militzer <dyotson@MacBook-Air-de-Maximiliano.local>
2023-06-26 14:29:08 -0400

    Added como_funciona and colaboradores page

--- DatosUC/main/urls.py
+++ DatosUC/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("datos/elecciones_feuc", views.elecciones_feuc, name="elecciones_feuc"),
     path("datos/votaciones_feuc", views.votaciones_feuc, name="votaciones_feuc"),
+    path("colaboradores", views.colaboradores, name="colaboradores"),
+    path("como_funciona", views.como_funciona, name="como_funciona"),
 ]
--- DatosUC/main/views.py
+++ DatosUC/main/views.py
@@ -19,7 +19,7 @@ def elecciones_feuc(request):
         "elecciones": elecciones,
         "anos": anos,
     }
-    return render(request, "main/elecciones_feuc.html", context)
+    return render(request, "main/elecciones/elecciones_feuc.html", context)
 
 
 def votaciones_feuc(request):
@@ -29,4 +29,12 @@ def votaciones_feuc(request):
         "votaciones": votaciones_feuc,
         "anos": anos,
     }
-    return render(request, "main/votaciones_feuc.html", context)
+    return render(request, "main/votaciones/votaciones_feuc.html", context)
+
+
+def colaboradores(request):
+    return render(request, "main/colaboradores.html")
+
+
+def como_funciona(request):
+    return render(request, "main/como_funciona.html")
