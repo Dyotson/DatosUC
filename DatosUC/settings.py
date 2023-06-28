37cc33f766b08c2228a75783364e9e48d8e97f25
Maximiliano Militzer <dyotson@MacBook-Air-de-Maximiliano.local>
2023-06-27 21:01:01 -0400

    Let's start debbugging... FUCK

--- DatosUC/settings.py
+++ DatosUC/settings.py
@@ -134,6 +134,6 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
